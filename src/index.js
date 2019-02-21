d7dc9a537e890d5c74879f1b42b3a55b0de2d469
skysharkk <sky.light.blr@gmail.com>
2019-02-21 22:05:33 +0300

    done

--- src/index.js
+++ src/index.js
@@ -4,4 +4,16 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+let count = 0;
+let firstLover;
+let secondLover;
+for(let i = 0; i <= preferences.length - 1; i++){
+    
+    firstLover = preferences[i];
+    secondLover = preferences[firstLover - 1];
+    if (preferences[secondLover - 1] === i + 1){
+        count++;        
+    }
+}
+return Math.floor(count / 3);
 };
